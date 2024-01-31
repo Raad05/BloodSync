f4b2c5929a325120e30a8ec52eec3bcf0ad3441d
Raad05 <ashabulraad@gmail.com>
2024-01-31 21:54:34 +0600

    landing page intro

--- app/landingPage/page.tsx
+++ app/landingPage/page.tsx
@@ -3,7 +3,10 @@ import React from "react";
 const LandingPage = () => {
   return (
     <div className="landing-page">
-      <h1>Landing Page</h1>
+      <div className="flex justify-around">
+        <div className="w-1/2 mx-10">Intro</div>
+        <div className="w-1/2 mx-10">SVG</div>
+      </div>
     </div>
   );
 };
