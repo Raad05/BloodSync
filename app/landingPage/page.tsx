b99c58487cb7f8f21faa0d888081eba2e49864c9
Abdul Hamid Mahi <87108050+AHMahi@users.noreply.github.com>
2024-02-12 02:36:22 +1100

    hero-section update

--- app/landingPage/page.tsx
+++ app/landingPage/page.tsx
@@ -1,10 +1,29 @@
 import React from "react";
 import "./landingpage.css"
+import Image from "next/image";
+import AuthBtn from "../components/AuthBtn/AuthBtn";
 
 const LandingPage = () => {
   return (
-    <div className="landing-page">
-      <h1 className="landing-page-title"><span className="cou">Courage</span> flows in your veins</h1>
+    <div className="landing-page grid grid-cols-2 gap-4">
+      <div className="landing-page-text-container">
+        <h4 className="landing-plus">+</h4>
+        <h1 className="landing-page-title-first">
+          <span className="title-courage">Courage</span> flows in
+        </h1>
+        <h1 className="landing-page-title-second">your veins</h1>
+        <div className="nav-button">
+        <AuthBtn></AuthBtn>
+        </div>
+      </div>
+      <div className="landing-page-image-container relative h-[500px] w-full"> {/* Adjust the height as needed */}
+        <Image 
+          src="/blood_donation.jpg" 
+          alt="Blood Donation" 
+          layout="fill" 
+          objectFit="cover"
+        />
+      </div>
     </div>
   );
 };
